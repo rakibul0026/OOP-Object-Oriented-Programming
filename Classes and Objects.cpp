aefd7d11ca572fcd48cf852ea689c7896ad2d630
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-26 00:25:08 +0600

    Update Classes and Objects.cpp

--- Classes and Objects.cpp
+++ Classes and Objects.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+class student
+{
+public:
+    int id;
+    double gpa;
+};
+int main()
+{
+    student Alim,Antora;
+    Alim.id=101;
+    Alim.gpa=3.1516;
+    cout<<Alim.id<<"  "<<Alim.gpa<<endl;
+
+
+    Antora.id=101;
+    Antora.gpa=3.1516;
+    cout<<Antora id<<"  "<<Antora.gpa<<endl;
+
+}
 
