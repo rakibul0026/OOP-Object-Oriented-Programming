e43bf94aa1cedbea0b41ee0acaee8e244c68b2a6
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-26 00:36:06 +0600

    Update Constructor.cpp

--- constructor and destructor/Constructor.cpp
+++ constructor and destructor/Constructor.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+class Addition
+{
+public :
+    int p,q,c;
+    Addition()
+    {
+        cin>>p>>q;
+        c=p+q;
+        cout<< "the result is: "<<c;
+
+    }
+};
+main()
+{
+
+  Addition ob;
+}
+
 
