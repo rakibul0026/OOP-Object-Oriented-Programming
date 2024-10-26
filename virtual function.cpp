bc66b44dc074ea8de2533903a03af5fd5fa14f3c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-26 08:25:45 +0600

    Update virtual function.cpp

--- virtual function.cpp
+++ virtual function.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
+class  Base
+{
+public:
+    virtual void print()
+    {
+        cout<<"Base  function"<<endl;
+    }
+};
+class Derived: public Base
+{
+public :
+    void print (){
+    cout<<"Derived Function"<<endl;
+
+    }
+};
+int main()
+{
+
+
+Derived derived1;
+Base*Base=&derived;
+base->print();
+return 0;
+}
 
