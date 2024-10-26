f99b858c0865f3bebe28f7582e607e07db069fc2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-26 08:43:15 +0600

    Update 2.overloading Binary Operator.cpp

--- Operator Overloading/2.overloading Binary Operator.cpp
+++ Operator Overloading/2.overloading Binary Operator.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class demo {
+    int a, b;
+public:
+    demo(int x, int y) {
+        a = x;
+        b = y;
+    }
+    void show() {
+        cout << "a: " << a << endl;
+        cout << "b: " << b << endl;
+    }
+
+    demo operator+(demo obj) {
+        demo temp(0,0);
+        temp.a = a + obj.a;
+        temp.b = b + obj.b;
+        return temp;
+    }
+};
+
+int main() {
+    demo ob(10, 20), ob1(30, 40), ob2(0,0);
+    ob2 = ob + ob1;
+    ob2.show();
+    return 0;
+}
 
