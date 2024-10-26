ce11e96490c05f014be375947a8cfdbacd662390
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-26 08:27:22 +0600

    Update Destructor.cpp

--- constructor and destructor/Destructor.cpp
+++ constructor and destructor/Destructor.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+class CSTU
+{
+public:
+    CSTU()
+    {
+        cout<<"this is constructor\n";
+    }
+    ~ CSTU()
+    {
+        cout<<"this is Disconstructor\n";
+    }
+
+};
+main()
+{
+    CSTU t;
+}
 
