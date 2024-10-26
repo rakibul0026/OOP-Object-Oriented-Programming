b12c8c105362377d806c1078a9c922ca58b9792e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-26 08:40:16 +0600

    Update overloading Unary Operator.cpp

--- Operator Overloading/overloading Unary Operator.cpp
+++ Operator Overloading/overloading Unary Operator.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class demo {
+    int a, b;
+public:
+    demo(int x, int y) {
+        a = x;
+        b = y;
+    }
+    void show() {
+        cout << "a: " << a << endl;
+        cout << "b: " << b << endl;
+    }
+    void operator-() {
+        a = -a;
+        b = -b; // Corrected this line
+    }
+};
+
+int main() {
+    demo ob(10, 20);
+    ob.show();
+    -ob;
+    ob.show();
+    return 0;
+}
 
