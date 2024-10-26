aa0091feb5b2066a1d147463a5293aee0b30b52b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-26 08:39:17 +0600

    Update overloading Binary Operator.cpp

--- Operator Overloading/overloading Binary Operator.cpp
+++ Operator Overloading/overloading Binary Operator.cpp
@@ -1,1 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Number {
+    int x, y;  // private member variables
+public:
+    Number() {
+        x = 0;
+        y = 0;
+    }
+    Number(int a, int b) {
+        x = a;
+        y = b;
+    }
+    void get(int &a, int &b) {
+        a = x;
+        b = y;
+    }
+    void set(int a, int b) {
+        x = a;
+        y = b;
+    }
+    void print() {
+        cout << x << " ";
+        cout << y << endl;
+    }
+    Number operator+(Number ob); // Operator overloading declaration
+};
+
+Number Number::operator+(Number ob) {
+    Number tmp;
+    tmp.x = x + ob.x;
+    tmp.y = y + ob.y;
+    return tmp;
+}
+
+int main() {
+    Number n1(4, 6), n2(5, 7), n3(8, 0);
+    n3 = n1 + n2;
+    n3.print(); // Output should be 9 13
+    return 0;
+}
 
