b4dc585ee289963e38a5806d03cdae4a2f9c0ab8
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-27 08:09:31 +0600

    Update 1.run time polymorphism.cpp

--- Polymorphism/1.run time polymorphism.cpp
+++ Polymorphism/1.run time polymorphism.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 using namespace std;
 
-class Animal {
+class Shape
+{
 public:
-    virtual void makeSound() {
-    cout << "Animal sound" << endl;
+    virtual void draw() // Virtual function for runtime polymorphism
+    {
+        cout << "Drawing a generic shape" << endl;
     }
 };
 
-class Dog : public Animal {
+class Circle : public Shape
+{
 public:
-    void makeSound() override {
-        cout << "Woof!" <<endl;
+    void draw() override // Overriding the base class function
+    {
+        cout << "Drawing a circle" << endl;
     }
 };
 
-int main() {
-    Animal* ptr1 = new Animal(); // Pointer to base class object
-    Animal* ptr2 = new Dog();    // Pointer to derived class object
+class Rectangle : public Shape
+{
+public:
+    void draw() override // Overriding the base class function
+    {
+        cout << "Drawing a rectangle" << endl;
+    }
+};
 
-    ptr1->makeSound(); // Output: "Animal sound"
-    ptr2->makeSound(); // Output: "Woof!"
+int main()
+{
+    Shape* shape1 = new Circle();     // Pointer to base class holding derived class object
+    Shape* shape2 = new Rectangle();  // Pointer to base class holding another derived class object
+    
+    shape1->draw(); // Calls Circle's draw()
+    shape2->draw(); // Calls Rectangle's draw()
 
-    /*delete ptr1;
-    delete ptr2;*/
+    delete shape1;
+    delete shape2;
+    
     return 0;
 }
-
