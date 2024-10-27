fb61d4b5e8b2648cfeb8bb67f8b9a6ef9216842e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-27 07:51:17 +0600

    Update 4. Unary operator with friend oprator.cpp

--- Operator Overloading/4. Unary operator with friend oprator.cpp
+++ Operator Overloading/4. Unary operator with friend oprator.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class demo {
+    int a, b;
+public:
+    demo() {}
+
+    demo(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    void show() {
+        cout << "a: " << a << endl;
+        cout << "b: " << b << endl;
+    }
+
+    friend demo operator+(const demo &obj, const demo &obj2);
+};
+
+demo operator+(const demo &obj, const demo &obj2) {
+    demo temp;
+    temp.a = obj.a + obj2.a;
+    temp.b = obj.b + obj2.b;
+    return temp;
+}
+
+int main() {
+    demo ob(10, 20), ob1(30, 40), ob2;
+    ob2 = ob + ob1;
+    ob2.show();
+    return 0;
+}
 
