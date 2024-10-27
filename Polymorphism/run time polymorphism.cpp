6e805ca949d84dcb4d49742d84c3b17bb63683d7
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-27 07:56:59 +0600

    Update run time polymorphism.cpp

--- Polymorphism/run time polymorphism.cpp
+++ Polymorphism/run time polymorphism.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void makeSound() {
+    cout << "Animal sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void makeSound() override {
+        cout << "Woof!" <<endl;
+    }
+};
+
+int main() {
+    Animal* ptr1 = new Animal(); // Pointer to base class object
+    Animal* ptr2 = new Dog();    // Pointer to derived class object
+
+    ptr1->makeSound(); // Output: "Animal sound"
+    ptr2->makeSound(); // Output: "Woof!"
+
+    /*delete ptr1;
+    delete ptr2;*/
+    return 0;
+}
 
