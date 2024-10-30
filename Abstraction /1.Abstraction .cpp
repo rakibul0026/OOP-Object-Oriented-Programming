adaced20f283e8a93a057fb85477d897e659c206
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 09:51:06 +0600

    Update and rename Abstraction .cpp to 1.Abstraction .cpp

---
+++ Abstraction /1.Abstraction .cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class MobileUser
+{
+public :
+    virtual void sendMassage()=0;
+    void call()
+    {
+        cout<<"Hellow"<<endl;
+    }
+};
+
+class Rahim : public MobileUser
+{
+    void sendMassage()
+    {
+
+        cout<<"Hi this is rahim"<<endl;
+    }
+
+};
+
+class karim:public MobileUser
+{
+    void sendMassage()
+    {
+
+        cout<<"Hi this is kahim"<<endl;
+    }
+
+};
+int main()
+{
+    MobileUser *m;
+
+    Rahim ob1;
+    karim ob2;
+
+    m=&ob1;
+    m->sendMassage();
+
+    m=&ob2;
+    m->sendMassage();
+
+
+}
+
