3a154897a2c75f5fd0081e6e19d74e230c3b3d1a
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 10:13:06 +0600

    Update GOBAL ANF LOCAL VARIABLE.cpp

--- Classes and Objects/GOBAL ANF LOCAL VARIABLE.cpp
+++ Classes and Objects/GOBAL ANF LOCAL VARIABLE.cpp
@@ -1,1 +1,18 @@
+//GOBAL ANF LOCAL VARIABLE
+#include<iostream>
+using namespace std;
+int glo=6;
+void sum(){
+    int a;
+
+//int a;
+
+
+}
+int main()
+{
+    int glo=9; //local variable
+    cout<<a;
+
+}
 
