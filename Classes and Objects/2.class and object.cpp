6ebd826427d3d8b50b18b3e303aa39c90486d5d9
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 11:07:33 +0600

    Update 2.class and object.cpp

--- Classes and Objects/2.class and object.cpp
+++ Classes and Objects/2.class and object.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
+class swap
+{
+    int n1,n2,temp;
+public:
+    void take()
+    {
+        cout<<"Enter two number";
+        cin>>n1>>n2;
+    }
+    void get()
+    {
+        temp=n1;
+        n1=n2;
+        n2=temp;
+        cout<<n1<<" "<<n2;
+    }
+};
+int main()
+{
+    swap obj;
+    obj.take();
+    obj.get();
+
+}
+
 
