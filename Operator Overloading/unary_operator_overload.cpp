a73a909a75804e4029378a2096df5d3d1065ba44
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 11:28:02 +0600

    Update unary_operator_overload.cpp

--- Operator Overloading/unary_operator_overload.cpp
+++ Operator Overloading/unary_operator_overload.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+using namespace std;
+class space
+{
+    int x,y;
+public:
+    void getdata(int a,int b);
+    void display(void);
+    void operator-();
+};
+void space:: getdata(int a,int b)
+{
+    x=a;
+    y=b;
+
+}
+void space:: display(void)
+
+{
+ cout<<"x="<<x<<endl;
+cout<<"y="<<y<<endl;
+}
+void space:: operator-()
+{
+    x=-x;
+    y=-y;
+}
+int main()
+{
+    space s;
+    s.getdata(6,8);
+    cout<<"s:";
+   s. display();
+   -s;
+
+   cout<<"-s:";
+    s. display();
+
+}
 
