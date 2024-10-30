bbe5a5b1cb98aa2edc1e986ca4bd768ed4e2df33
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 11:32:50 +0600

    Update 3.multiple_inheritance_virtual.cpp

--- Inheritance/3.multiple_inheritance_virtual.cpp
+++ Inheritance/3.multiple_inheritance_virtual.cpp
@@ -1,1 +1,37 @@
+# include <iostream >
+using namespace std;
+class base
+{
+public :
+    int i;
+};
+
+class derived1 : virtual public base
+{
+public :
+    int j;
+};
+
+class derived2 : virtual public base
+{
+public :
+    int k;
+};
+class derived3 : public derived1, public derived2
+{
+public :
+    int product ()
+    {
+        return i * j * k;
+    }
+};
+int main ()
+{
+    derived3 ob;
+    ob.i = 10; 
+    ob.j = 3;
+    ob.k = 5;
+    cout << " Product is " << ob. product () << "\n";
+    return 0;
+}
 
