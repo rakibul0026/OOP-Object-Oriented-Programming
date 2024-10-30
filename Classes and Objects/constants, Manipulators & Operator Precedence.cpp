2f3abd5944c3454408859c1a01870d7b25e00942
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 10:09:33 +0600

    Update constants, Manipulators & Operator Precedence.cpp

--- Classes and Objects/constants, Manipulators & Operator Precedence.cpp
+++ Classes and Objects/constants, Manipulators & Operator Precedence.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+   /* const int a=8;
+    a=5;
+    cout<<"c+"<<a;
+    return 0;*/
+   // int a=3,b=87,c=123;
+    //Manipulators in c++
+      /*  cout<<"the value of a is:"<<a<<endl;
+            cout<<"the value of a is:"<<b<<endl;
+               cout<<"the value of a is:"<<c<<endl;
+        cout<<"the value of a is:"<<setw(6)<<a<<endl;
+           cout<<"the value of b is:"<<setw(8)<<b<<endl;
+            cout<<"the value of c is:"<<setw(9)<<c<<endl;  */
+          //  operator Precedence in c++
+          int a=3,b=4;
+          int c=(a*5)+b;
+          cout<<c;
+          return 0;
+
+
+
+}
 
