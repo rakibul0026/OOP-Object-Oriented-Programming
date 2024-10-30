0c85a36890ac4824117c68fe0f6ca30598607e56
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 09:58:53 +0600

    Update 3.overloading Binary Operator.cpp

--- Operator Overloading/3.overloading Binary Operator.cpp
+++ Operator Overloading/3.overloading Binary Operator.cpp
@@ -1,1 +1,44 @@
+#include<iostream>
+using namespace std;
+class test
+{
+    int a,b;
+public :
+    test()
+    {
+        a=0;
+        b=0;
+    }
+    test(int x, int y)
+    {
+        a=x;
+        b=y;
+    }
+    test operator+(test t);
+    void disp()
+};
+test test:: operator+(test t)
+{
+    test temp;
+    temp.a=a+t.a;
+    temp=b+ t.b;
+    return  temp;
+}
+void  test::dis()
+{
+    cout << "\n value of A" << a endl;
+    cout << "\n value of B" << b endl;
+
+}
+int main()
+{
+    test t1,t2,t3;
+    t1=test(10,20);
+    t2=test(30,40);
+    t3=t1+t2;
+    t1.dis();
+    t2.dis();
+    t3.dis();
+    return 0;
+}
 
