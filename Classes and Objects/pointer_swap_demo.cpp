d3ab61e8b01272c0aa88082db583a80f930de83e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 10:16:34 +0600

    Update pointer_swap_demo.cpp

--- Classes and Objects/pointer_swap_demo.cpp
+++ Classes and Objects/pointer_swap_demo.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+void swap(int *x,int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a and b:";
+    cin>>a;
+    cin>>b;
+    swap(&a,&b);
+    cout<<"a:"<<a<<endl;
+    cout<<"b:"<<b;
+}
 
