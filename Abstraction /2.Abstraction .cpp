475f718948dd0c2f1f9130627782865b46a24ba8
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-30 09:52:05 +0600

    Update 2.Abstraction .cpp

--- Abstraction /2.Abstraction .cpp
+++ Abstraction /2.Abstraction .cpp
@@ -1,1 +1,39 @@
 
+#include<iostream>
+using namespace std;
+class MobileUser
+{
+public:
+    void call()
+    {
+        cout<<"hellow!"<<endl;  //No virtual function
+    }
+    virtual void sendMessage()=0;
+
+};
+
+class kahim : public MobileUser
+{
+    void sendMessage()
+    {
+
+        cout<<"hi,love for this\n";
+
+    }
+};
+int main()
+MobileUser *m;
+Rahim r;
+Karim k;
+m=&r;
+m->sendMessage();
+
+
+
+}
+
+
+
+
+
+
