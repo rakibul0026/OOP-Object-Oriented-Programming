2e8b5019868f606c49f0d2125b91d75f607bb254
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-31 12:31:55 +0600

    Update Amiguity.cpp

--- 1.C++ Basic Syntex/Amiguity.cpp
+++ 1.C++ Basic Syntex/Amiguity.cpp
@@ -1,1 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Base1
+{
+public :
+    void greed()
+    {
+        cout<<"How are you"<<endl;
+
+    }
+};
+class Base2
+{
+public :
+    void greed()
+    {
+        cout<<"how old are you "<<endl;
+
+    }
+};
+class Derived : public Base1,public Base2
+{
+public :
+    void greed()
+    {
+        Base2::greed();
+    }
+
+
+};
+int main()
+{
+
+    Base1 ob1;
+    ob1.greed();
+
+    Base2 ob2;
+    ob2.greed();
+
+    Derived d;
+    d.greed();
+
+
+    return  0;
+
+}
 
