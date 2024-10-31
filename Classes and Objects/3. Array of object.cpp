1fc568d904f8ea2bce9f9b611c64df686a92e763
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-31 12:26:12 +0600

    Update 3. Array of object.cpp

--- Classes and Objects/3. Array of object.cpp
+++ Classes and Objects/3. Array of object.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+class A
+{
+public :
+    void print()
+    {
+        cout<<"Wellcome to Bangladesh"<<endl;
+    }
+
+};
+int main()
+{
+    A obj[3];
+    for(int i=0; i<3; i++)
+    {
+        obj[i].print();
+
+    }
+
+
+}
 
