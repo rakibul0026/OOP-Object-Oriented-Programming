024232e9a341f2e185c1c34f0dce089b508824af
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-31 12:29:18 +0600

    Update 1.armstrong.cpp

--- 1.C++ Basic Syntex/1.armstrong.cpp
+++ 1.C++ Basic Syntex/1.armstrong.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,num,temp,r,sum=0;
+    for(i=1;i<=10;i++)
+    {
+        temp=i;
+        while(temp!=0)
+        {
+            r=temp%10;
+            sum=sum+r*r*r;
+            temp=temp/10;
+
+        }
+        if(sum=i){
+            cout<<sum<<endl;
+        }
+    }
+}
 
