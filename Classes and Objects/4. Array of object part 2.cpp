e0337c4f5d3c7d00458a21a28a169acabe72d630
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-31 12:27:48 +0600

    Update 4. Array of object part 2.cpp

--- Classes and Objects/4. Array of object part 2.cpp
+++ Classes and Objects/4. Array of object part 2.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int longestGoodArrayLength(int l, int r)
+{
+    long long range_length = r - l;
+
+
+    int k = (int)floor(sqrt(2 * range_length + 0.25) - 0.5);
+
+
+    while ((k * (k + 1)) / 2 > range_length + 1)
+    {
+        k--;
+    }
+
+    return k + 1;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        cout << longestGoodArrayLength(l, r) << endl;
+    }
+    return 0;
+}
 
