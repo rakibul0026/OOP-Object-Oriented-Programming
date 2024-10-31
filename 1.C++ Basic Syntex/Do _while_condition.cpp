5a5dda2bef4e19b511df1fe9452fa02b77bed653
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-31 12:41:36 +0600

    Update Do _while_condition.cpp

--- 1.C++ Basic Syntex/Do _while_condition.cpp
+++ 1.C++ Basic Syntex/Do _while_condition.cpp
@@ -1,1 +1,16 @@
 
+# include <iostream >
+using namespace std;
+int main ()
+{
+    char ch;
+    cout << " Enter keys x to stop \n";
+    do
+    {
+        cout << ": ";
+        cin >> ch;
+    }
+    while (ch!= 0);
+    return 0;
+}
+
