b313f9d6d69c32ac275d2672f0e7b64b102c15cd
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-31 11:58:31 +0600

    Update 2.function Templates.cpp

--- Templates/2.function Templates.cpp
+++ Templates/2.function Templates.cpp
@@ -1,1 +1,17 @@
+// function Templates
+#include<iostream>
+#include<conio.h>
+using namespace std;
+template<class T>
+T add(T a,T b)
+{
+
+    return a+b;
+}
+int main()
+{
+    cout<<add(10,20)<<endl;
+    cout<<add(10.5,20.2)<<endl;
+
+}
 
