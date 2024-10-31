81c9db43c0a9e7c1e54ba3d3ebfd0eee8bd7e278
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-31 12:36:58 +0600

    Update condition.cpp

--- 1.C++ Basic Syntex/condition.cpp
+++ 1.C++ Basic Syntex/condition.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<" Enter the age:";
+    cin>>age;
+    cout<<age;
+    if(age<18)
+    {
+        cout<<"Bangladesh is development country";
+    }
+
+    else if  (age>25)
+    {
+        cout<<"Bangladesh is wounderful country";
+    }
+
+    else if  (age>55)
+    {
+        cout<<"Bangladesh is wounderful country";
+    }
+    return 0;
+
+}
+
 
