1985be077dfc48a572d79ed53874becefe759b41
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-01 15:22:29 +0600

    Update scope regulation.cpp

--- 1.C++ Basic Syntex/scope regulation.cpp
+++ 1.C++ Basic Syntex/scope regulation.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float x=0026;
+    float &y=x;
+    cout<<x<<endl;
+    cout<<y;
+    /*type casting*/
+    int a=80.4545;
+    cout<<"\n The value of a is:"<<(int)a;
+}
 
