fb03ca5fa16ce20e50351d955142da7a77dffe1c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-01 16:00:38 +0600

    Update 2.call by reference.cpp

--- Call by value and call by reference in C++/2.call by reference.cpp
+++ Call by value and call by reference in C++/2.call by reference.cpp
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
 
+// call by reference
+void modifyValue(int *a, int *b)
+{
+
+    *a = 15;
+    *b = 25;
+}
+
+int main()
+{
+
+    int x = 10, y = 20;
+    modifyValue(&x, &y);
+    printf("Call by Value:\n");
+    printf("x = %d, y = %d\n\n", x, y);
+
+    return 0;
+}
