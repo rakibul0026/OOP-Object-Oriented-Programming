2d9df6005cb0054f7aad5f37b90ead6cd7d85c39
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-01 16:15:34 +0600

    Update stracture, Union &Enums inc++.cpp

--- 1.C++ Basic Syntex/stracture, Union &Enums inc++.cpp
+++ 1.C++ Basic Syntex/stracture, Union &Enums inc++.cpp
@@ -1,1 +1,37 @@
 
+#include<iostream>
+using namespace std;
+struct employee
+{
+    int eId;
+    char favChar;
+    long  double salary;
+};
+
+int main()
+{
+    struct employee harry;
+    harry.eId=1;
+    harry.favChar='c';
+    harry.salary=1200000;
+
+    struct employee borsha;
+
+    borsha.eId=6;
+    borsha.favChar='tu';
+    borsha.salary=1000000;
+
+    /*struct employee torju;
+    harry.eId;
+    harry.favChar;
+    harry .salary;*/
+    cout<<"the value  is:"<<harry.eId<<endl;
+    cout<<"the value  is:"<<harry.favChar<<endl;
+    cout<<"the value  is:"<<harry.salary<<endl;
+    cout<<"the value  is:"<<borsha.eId<<endl;
+    cout<<"the value  is:"<<borsha.favChar<<endl;
+    cout<<"the value  is:"<<borsha.salary<<endl;
+    return 0;
+
+}
+
