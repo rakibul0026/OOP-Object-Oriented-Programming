6f0e3573c5aeeb0863a86d66348e5fcb146b4015
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-01 16:13:33 +0600

    Create simple calculator.cpp

---
+++ 1.C++ Basic Syntex/simple calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int X,Y,sum;
+    cin>>X>>Y;
+    sum=X+Y;
+    int  mul=X*Y;
+    int sub=X-Y;
+    cout<<X<<"+"<<Y<<"="<<sum<<endl;
+    cout<<X<<"*"<<Y<<"="<<mul<<endl;
+    cout<<X<<"-"<<Y<<"="<<sub<<endl;
+
+    return 0;
+}
