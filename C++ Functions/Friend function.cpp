2b2b5668544ec0e1337c16e5590a7c55ab3496ee
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-01 15:26:07 +0600

    Update Friend function.cpp

--- C++ Functions/Friend function.cpp
+++ C++ Functions/Friend function.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Numbers {
+    int a, b;
+
+public:
+   Numbers(int x, int y) : a(x), b(y) {}
+    friend int product(Numbers);
+};
+
+int product(Numbers num) {
+    return num.a * num.b;
+}
+
+int main() {
+    int a, b;
+    cout << "Enter the values of a and b: ";
+    cin >> a >> b;
+    Numbers num(a, b);
+
+    cout << "The product of a and b: " << product(num) << endl;
+    cout << "The product of a and b: " << product(num) << endl;
+    cout << "The product of a and b: " << product(num) << endl;
+    cout << "The product of a and b: " << product(num) << endl;
+
+    return 0;
+}
 
