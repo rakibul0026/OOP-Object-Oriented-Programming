9c1a5b66719daebfe23f8462bcd3422f69dcf056
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-02 12:49:11 +0600

    Update 2.Multilevel inheritance.cpp

--- Inheritance/2.Multilevel inheritance.cpp
+++ Inheritance/2.Multilevel inheritance.cpp
@@ -1,1 +1,37 @@
 
+#include <iostream>
+using namespace std;
+
+class base {
+public:
+    base() {
+        cout << "Constructing base class\n";
+    }
+    ~base() {
+        cout << "Destructing base class\n";
+    }
+};
+
+class derived1 : public base { 
+    derived1() {
+        cout << "Constructing derived class 1\n";
+    }
+    ~derived1() {
+        cout << "Destructing derived class 1\n";
+    }
+};
+
+class derived2 : public derived1 { 
+public:
+    derived2() {
+        cout << "Constructing derived class 2\n";
+    }
+    ~derived2() {
+        cout << "Destructing derived class 2\n";
+    }
+};
+
+int main() {
+    derived2 obj;  
+    return 0;
+}
