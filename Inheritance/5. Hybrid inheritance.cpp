70be396dd9fc7fff2f4dc20f135d4417d68eab1c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-02 12:56:21 +0600

    Update 5. Hybrid inheritance.cpp

--- Inheritance/5. Hybrid inheritance.cpp
+++ Inheritance/5. Hybrid inheritance.cpp
@@ -1,1 +1,47 @@
 
+#include <iostream>
+using namespace std;
+
+class base {
+public:
+    base() {
+        cout << "Constructing base class\n";
+    }
+    ~base() {
+        cout << "Destructing base class\n";
+    }
+};
+class derived1 : public base {
+public:
+    derived1() {
+        cout << "Constructing derived class 1\n";
+    }
+    ~derived1() {
+        cout << "Destructing derived class 1\n";
+    }
+};
+
+class derived2 : public base {
+public:
+    derived2() {
+        cout << "Constructing derived class 2\n";
+    }
+    ~derived2() {
+        cout << "Destructing derived class 2\n";
+    }
+};
+
+class hybrid : public derived1, public derived2 {
+public:
+    hybrid() {
+        cout << "Constructing hybrid class\n";
+    }
+    ~hybrid() {
+        cout << "Destructing hybrid class\n";
+    }
+};
+
+int main() {
+    hybrid obj;  
+    return 0;
+}
