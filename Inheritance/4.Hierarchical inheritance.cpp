9d5f8f98e61c732e7786adbd20be14d8a5029261
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-02 12:54:33 +0600

    Update 4.Hierarchical inheritance.cpp

--- Inheritance/4.Hierarchical inheritance.cpp
+++ Inheritance/4.Hierarchical inheritance.cpp
@@ -1,1 +1,50 @@
 
+#include <iostream>
+using namespace std;
+
+class base {
+public:
+    base() {
+        cout << "Constructing base class\n";
+    }
+    ~base() {
+        cout << "Destructing base class\n";
+    }
+};
+
+class derived1 : public base { 
+public:
+    derived1() {
+        cout << "Constructing derived class 1\n";
+    }
+    ~derived1() {
+        cout << "Destructing derived class 1\n";
+    }
+};
+
+class derived2 : public base {  
+public:
+    derived2() {
+        cout << "Constructing derived class 2\n";
+    }
+    ~derived2() {
+        cout << "Destructing derived class 2\n";
+    }
+};
+
+class derived3 : public base { 
+public:
+    derived3() {
+        cout << "Constructing derived class 3\n";
+    }
+    ~derived3() {
+        cout << "Destructing derived class 3\n";
+    }
+};
+
+int main() {
+    derived1 obj1; 
+    derived2 obj2;  
+    derived3 obj3; 
+    return 0;
+}
